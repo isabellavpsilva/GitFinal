b1bd6fdbd83c7b813266dd83771d39b3eae94046
isabellavpsilva <isabellavpsilva@gmail.com>
2023-03-24 10:19:42 -0400

    second commit

--- src/finalProject/FinalClass.java
+++ src/finalProject/FinalClass.java
@@ -1,5 +1,9 @@
 package finalProject;
 
-public class FinalClass {
+public class FinalClass {public static void main(String[] args) {
+	
+	
+	System.out.println("this is the code for mercedes");
+}
 
 }
